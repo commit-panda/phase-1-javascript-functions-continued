37219afef1cc95d0f5f8ffeb50681d0486d321a8
commit-panda <sjmanyarkiy@gmail.com>
2025-10-01 17:35:17 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate"){
+   
+    return `This Saturday, I want to ${activity}!`;
+}
+
+
+const mondayWork = function(activity2 = "go to the office"){
+    return `This Monday, I will ${activity2}.`;
+}
+
+
+
+function wrapAdjective(wrapper = "*") {
+  return function(adjective = "a hard worker") {
+    return `You are ${wrapper}${adjective}${wrapper}!`;
+  };
+}
+
+
